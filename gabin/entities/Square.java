bdfd7d64746257c52377883923da1696f31da791
mutuzogabin <shemamutuzogabin250@gmail.com>
2025-09-19 17:51:37 +0200

    Add files via upload

---
+++ gabin/entities/Square.java
@@ -0,0 +1,14 @@
+package gabin.entities;
+
+public class Square {
+    private final double side;
+    public Square(double side) {
+        this.side = side;
+    }
+    public double calculatePerimeter() {
+        return  side * 4;
+    }
+    public double calculateArea() {
+        return side * side;
+    }
+}
